b5cf6c028ad928507f267addba8e66bbc2335211
vishal singh <tsinghvishall@gmail.com>
2025-02-09 14:01:35 +0530

    Fix: Update server.js for correct deployment settings

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL', 'https://strapi-server-app-0sr8.onrender.com'), // Add this line
   app: {
     keys: env.array('APP_KEYS'),
   },
